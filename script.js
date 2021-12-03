61db713fc5211089f01090847f7bc191c1aa2e2e
shailajaBegari <begari.shailaja21@gmail.com>
2021-12-03 16:42:52 +0530

    commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const dateOfBirth=document.querySelector("#date-of-birth")
+const luckyNumber=document.querySelector("#lucky-number")
+const checkNumberButton=document.querySelector("#check-number")
+const outputBox=document.querySelector("#output-box")
+ console.log(dateOfBirth)
+function compareValues(sum,luckyNumber){
+    if (sum%luckyNumber===0){
+        outputBox.innerText="WOW YOUR BIRTHDAY IS LUCKY NUMBER";
+    }else{
+        outputBox.innerText="OHH SORRY YOUR BIRTHDAY IS NOT A LUCKY NUMBER "
+    }
+}
+
+function checkBirthdayIsLucky(){
+    const dob=dateOfBirth.value
+    const sum=calculateSum(dob);
+    console.log(sum,dob)
+
+    if(sum&&dob){
+    compareValues(sum,luckyNumber.value)
+    }
+    else{
+    outputBox.innerText="please enter both the fields"
+    }
+    
+}
+function calculateSum(dob){
+    console.log(dob)
+    dob=dob.replaceAll("-","");
+    let sum=0;
+    for(let index=0;index<dob.length;index++){
+        sum=sum+Number(dob.charAt(index))
+    }
+    return sum
+}
+checkNumberButton.addEventListener("click",checkBirthdayIsLucky)
